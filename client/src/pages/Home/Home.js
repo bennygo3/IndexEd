3e1d30250dd9a50a294766d5907262ef1738b96e
bennygo3 <3gobenjamin@gmail.com>
2024-03-12 18:07:48 -0500

    home page changes. creating lines for background that creates a flashcard image

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,12 +3,29 @@ import './Home.css';
 import Navbar from '../../components/Navbar/Navbar';
 
 const Home = () => {
+  
+  const generateLines = (amount, colorClass) => {
+    const lines =[];
+    const spacing = 25;
+    for (let i = 0; i < amount; i++) {
+      const linePosition = 100 + i * spacing;
+      lines.push(
+        <div 
+          key={i} 
+          className={colorClass}
+          style={{ top: `${linePosition}px`}}
+        ></div>
+      );
+    }
+    return lines;
+  };
   return (
     <main className="home-body">
       <div className="home-container">
         <h1 className="home-header">Index<span style={{ marginLeft: "10px" }}></span>Ed</h1>
           <div className="home-background">
             <div className="red-line"></div>
+            {generateLines(17, "blue-line")}
             <Navbar />
       
           </div>
