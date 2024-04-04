ecfdd3affa8cc5c9ee95c4a1781ed68fa4731466
bennygo3 <3gobenjamin@gmail.com>
2024-04-04 12:45:42 -0500

    reworking the landing page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,11 +1,6 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
-// import highlighterLogin from '../../assets/highlighterLogin.png';
-// import highlighterSignUp from '../../assets/highlighterSignUp.png';
-
+import React, { useState } from 'react';
 import Login from '../../components/Login/Login.js';
 import SignUp from '../../components/SignUp/SignUp.js';
-import { useState } from 'react';
 import "./landing.css";
 
 function Landing() {
@@ -19,13 +14,11 @@ function Landing() {
                 <h1 className="landing-header">IndexEd</h1>
 
                 <div id="highlightLogin">
-                    {/* <img className="highlit" alt="decorative" src={highlighterLogin} /> */}
                     <div id="login" onClick={() => setLoginPopup(true)}> Login </div>
                     <Login trigger={loginPopup} setTrigger={setLoginPopup} />
 
                 </div>
                 <div id="highlightSignUp">
-                    {/* <img className="highlit" alt="decorative" src={highlighterSignUp} /> */}
                     <div id="signUp" onClick={() => setButtonPopup(true)}>Sign Up</div>
                     <SignUp trigger={buttonPopup} setTrigger={setButtonPopup} />
 
