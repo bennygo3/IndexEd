0dc4792d5431f7ac6fc1db2ffb855257e08ad6b3
bennygo3 <3gobenjamin@gmail.com>
2024-04-05 21:18:05 -0500

    server changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import { typeDefs, resolvers } from './schemas/index.js';
 import { authMiddleware } from './utils/auth.js';
 import db from './config/connection.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,6 +23,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     
@@ -29,10 +32,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, '../client/build/index.html'))
     });
 }
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'))
-// });
 
 await startStandaloneServer(server, {
     app,
