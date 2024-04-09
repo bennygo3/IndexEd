2919bfc8cf226dc9cb849965d4aaff83004a89f6
bennygo3 <3gobenjamin@gmail.com>
2024-04-09 16:24:26 -0500

    server

--- server/server.js
+++ server/server.js
@@ -30,12 +30,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    
-    app.get('*', (req, res) => {
-        console.log('Serving index.html for path:', req.path);
-        res.sendFile(path.join(__dirname, '../client/build/index.html'))
-    });
 }
+
+app.get('*', (req, res) => {
+    console.log('Serving index.html for path:', req.path);
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
 
 await startStandaloneServer(server, {
     app,
