c1105433e2568623d09b44707ee73d3060a453b7
bennygo3 <3gobenjamin@gmail.com>
2024-04-10 14:55:28 -0500

    more server reconfigs

--- server/server.js
+++ server/server.js
@@ -11,37 +11,38 @@ import path from 'path';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-
-    app.get('*', (req, res) => {
-        console.log('Serving index.html for path:', req.path);
-        res.sendFile(path.join(__dirname, '../client/build/index.html'))
-    });
-}
-
-await startStandaloneServer(server, {
-    app,
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
     context: ({ req }) => {
         const user = authMiddleware({ req }).user;
         return { user };
-    },
+    }
+});
+
+await startStandaloneServer(server, {
+    app,
     listen: { port: PORT },
 }).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`)
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.get('*', (req, res) => {
+        console.log('Serving index.html for path:', req.path);
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+}
 
 db.once('open', () => {
     console.log('MongoDB connected.')
