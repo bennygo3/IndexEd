eaad83de62b8288c1c4f6d7bff65701e461cb137
bennygo3 <3gobenjamin@gmail.com>
2024-04-11 13:09:22 -0500

    server reconfigured for express4

--- server/server.js
+++ server/server.js
@@ -1,49 +1,54 @@
 import 'dotenv/config';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { fileURLToPath } from 'url';
+import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authMiddleware } from './utils/auth.js';
 import db from './config/connection.js';
-import path from 'path';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const httpServer = http.createServer(app);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../client/build')));
-}
-
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
+// Create an instance of ApolloServer
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => {
-        const user = authMiddleware({ req }).user;
-        return { user };
-    }
 });
 
-await startStandaloneServer(server, {
-    app,
-    listen: { port: PORT },
-}).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`)
-});
+await server.start();
 
+// Apply middleware to the Express app
+app.use(
+    '/graphql',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+        context: ({ req }) => {
+            const user = authMiddleware({ req }).user;
+            return { user };
+        },
+    }),
+);
+
+// Serve static files and the React app
 if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
-        console.log('Serving index.html for path:', req.path);
-        res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+        res.sendFile(path.join(__dirname, '../client/build/index.html'))
     });
 }
 
+// Connect to the database
 db.once('open', () => {
     console.log('MongoDB connected.')
 });
+
+// Start the HTTP server
+await new Promise(resolve => httpServer.listen({ port: PORT }, resolve));
+console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
