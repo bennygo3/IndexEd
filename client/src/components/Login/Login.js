c7a490fffb71b6239bb33e317e0bd8a493097b6d
bennygo3 <3gobenjamin@gmail.com>
2024-04-12 15:32:24 -0500

    naming changes

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,7 +3,7 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 import StickyNote from '../StickyNote/StickyNote';
-import './login.css';
+import './Login.css';
 
 function Login(props) {
     const [formState, setFormState] = useState({
