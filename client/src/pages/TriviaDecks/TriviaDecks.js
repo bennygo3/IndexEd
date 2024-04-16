2a2ef30a4c53007f9347e26312104d762f85619b
bennygo3 <3gobenjamin@gmail.com>
2024-04-16 15:40:05 -0500

    fixed the linkUrl property/routing so that it will take users to the correct page

--- client/src/pages/TriviaDecks/TriviaDecks.js
+++ client/src/pages/TriviaDecks/TriviaDecks.js
@@ -2,14 +2,17 @@ import React from 'react';
 import TriviaCarousel from '../../components/TriviaCarousel/TriviaCarousel';
 
 const genres = [
-    { genre: 'Pokemon', description: 'Guess the name of Pokemon!', linkUrl: './TriviaGenres/Pokemon/PokemonTrivia.js'}
+    { genre: 'Pokemon', description: 'Guess the name of Pokemon!', linkUrl: '/pokemon-trivia'}
 ]
 
 const TriviaDecks = () => {
     return (
+        <>
+        <h1>Trivia</h1>
         <div className='trivia-decks'>
             <TriviaCarousel genres={genres} />
         </div>
+        </>
     )
 }
 
