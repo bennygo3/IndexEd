3e5499064c45cd8a02975032e6cf5a628575ee00
bennygo3 <3gobenjamin@gmail.com>
2024-04-29 16:37:02 -0500

    nba logos are showing one at a time from api call now. they also automatically flip back over to the front side when switching to a new card

--- client/src/pages/TriviaGenres/Sports/NBA/NbaLogos.js
+++ client/src/pages/TriviaGenres/Sports/NBA/NbaLogos.js
@@ -1,72 +1,137 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../../../../components/Card/Card.js';
+import { useSwipeable } from 'react-swipeable';
 import './nbaLogos.css'
 
 export default function NbaLogos() {
     const [nbaLogos, setNbaLogos] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [isFlipped, setIsFlipped] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-    const [flippedIndices, setFlippedIndices] = useState(new Set());
 
-    useEffect(() => {
-        const fetchNBAData = async () => {
-            setIsLoading(true);
-            setError(null);
-            try {
-                const response = await fetch('/api/nba-logos');
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                const data = await response.json();
-                if (data.body && Array.isArray(data.body)) {
-                    setNbaLogos(data.body.map(team => ({
-                        logo: team.nbaComLogo1,
-                        name: team.teamName
-                    })));
-                } else {
-                    throw new Error('Data format error, expected an array');
-                }
-            } catch (error) {
-                console.error('Failed to fetch NBA logos:', error);
-                setError(error.message);
-            }
-            setIsLoading(false);
-        };
+    const handlers = useSwipeable({
+        onSwipedLeft: () => updateIndex((activeIndex + 1) % nbaLogos.length),
+        onSwipedRight: () => updateIndex((activeIndex - 1 + nbaLogos.length) % nbaLogos.length),
+    });
 
-        fetchNBAData();
-    }, []); // Empty array ensures effect only runs after initial render
-
-    const toggleFlip = index => {
-        setFlippedIndices(currentIndices => {
-            const newIndices = new Set(currentIndices);
-            if (newIndices.has(index)) {
-                newIndices.delete(index);
-            } else {
-                newIndices.add(index);
-            }
-            return newIndices;
-        });
+    const updateIndex = (newIndex) => {
+        setIsFlipped(false); // 
+        setActiveIndex(newIndex);
     };
 
+    const toggleFlip = () => {
+        setIsFlipped(!isFlipped);
+    };
+
+    const fetchNBAData = async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('/api/nba-logos');
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log(data);
+            if (data.body && Array.isArray(data.body)) {
+                setNbaLogos(data.body.map(team => ({
+                    logo: team.nbaComLogo1,
+                    name: `${team.teamCity} ${team.teamName}`
+                })));
+            } else {
+                throw new Error('Data format error, expected an array');
+            }
+        } catch (error) {
+            console.error('Failed to fetch NBA logos:', error);
+            setError(error.message);
+        }
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        fetchNBAData();
+    }, []);
+
     return (
-        <div className='nba-logos-container'>
+        <>
             {isLoading ? (
                 <div>Loading...</div>
             ) : error ? (
                 <div className="error-message">Error: {error}</div> // Displaying the error message if present
             ) : (
-                <div className='cards-grid'>
-                    {nbaLogos.map((team, index) => (
-                        <div key={index} className='nba-logo-card' onClick={() => toggleFlip(index)}>
-                            <Card
-                                front={<img src={team.logo} alt={`${team.name} logo`} />}
-                                back={<div>{team.name}</div>}
-                                isFlipped={flippedIndices.has(index)}
-                            />
-                        </div>
-                    ))}
+                <div className='nba-logos-container' {...handlers}>
+                    <h1>NBA Team Logos</h1>
+                    <h3>...click on the NBA team logo to reveal the team name!</h3>
+                    <div className='nba-logos'>
+                        {nbaLogos.length > 0 && (
+                            <div className='nba-card-logo' onClick={toggleFlip}>
+                                <Card
+                                    front={<img src={nbaLogos[activeIndex].logo} alt={`${nbaLogos[activeIndex].name} logo`} />}
+                                    back={<div>{nbaLogos[activeIndex].name}</div>}
+                                    isFlipped={isFlipped}
+                                />
+                            </div>
+                        )}
+                    </div>
+
+                    <div className='carousel-controls'>
+                        <button onClick={() => updateIndex((activeIndex - 1 + nbaLogos.length) % nbaLogos.length)}>Previous</button>
+                        <button onClick={() => updateIndex((activeIndex + 1) % nbaLogos.length)}>Next</button>
+                    </div>
                 </div>
             )}
-        </div>
+
+        </>
     );
 }
+
+// useEffect(() => {
+//     const fetchNBAData = async () => {
+//         setIsLoading(true);
+//         setError(null);
+//         try {
+//             const response = await fetch('/api/nba-logos');
+//             if (!response.ok) {
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
+//             const data = await response.json();
+//             if (data.body && Array.isArray(data.body)) {
+//                 setNbaLogos(data.body.map(team => ({
+//                     logo: team.nbaComLogo1,
+//                     name: team.teamName
+//                 })));
+//             } else {
+//                 throw new Error('Data format error, expected an array');
+//             }
+//         } catch (error) {
+//             console.error('Failed to fetch NBA logos:', error);
+//             setError(error.message);
+//         }
+//         setIsLoading(false);
+//     };
+
+//     fetchNBAData();
+// }, []); // Empty array ensures effect only runs after initial render
+
+//     return (
+//     <div className='nba-logos-container' {...handlers}>
+//         {isLoading ? (
+//             <div>Loading...</div>
+//         ) : error ? (
+//             <div className="error-message">Error: {error}</div> // Displaying the error message if present
+//         ) : (
+//             <div className='cards-grid'>
+//                 {nbaLogos.map((team, index) => (
+//                     <div key={index} className='nba-logo-card' onClick={() => toggleFlip(index)}>
+//                         <Card
+//                             front={<img src={team.logo} alt={`${team.name} logo`} />}
+//                             back={<div>{team.name}</div>}
+//                             isFlipped={flippedIndices.has(index)}
+//                         />
+//                     </div>
+//                 ))}
+//             </div>
+//         )}
+//     </div>
+// );
