7655112f56849528bda24fb1463834676c589e4e
bennygo3 <3gobenjamin@gmail.com>
2024-05-13 11:33:28 -0500

    turning the fun fact component into a modal. adding props to the comp for different modal content on different pages

--- client/src/components/Facts/FunFacts.js
+++ client/src/components/Facts/FunFacts.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function FunFacts() {
+export default function FunFacts({ factTitle, factContent }) {
+    const [isOpen, factIsOpen] = useState(false);
+
+    const toggleModal = () => factIsOpen(!isOpen);
     return (
+        <>
         <div className='ff-container'>
             <h2 className="material-symbols-outlined">psychology_alt</h2>
             <p>Fun Fact!</p>
         </div>
-    )
+
+        {isOpen && (
+            <div>
+                
+            </div>
+        )}
+        </>
+    );
 }
