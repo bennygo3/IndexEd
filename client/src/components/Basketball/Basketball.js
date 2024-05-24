c191eabdd1434503e4cf45a7288f8d25591ee671
bennygo3 <3gobenjamin@gmail.com>
2024-05-24 16:36:41 -0500

    adding notes to pages. walking thru code

--- client/src/components/Basketball/Basketball.js
+++ client/src/components/Basketball/Basketball.js
@@ -2,9 +2,10 @@ import React, { useEffect, useRef } from 'react';
 import './basketball.css';
 
 export default function Basketball() {
-    const ballRef = useRef(null);
+    // useRef hook creates references to DOM elements
     const containerRef = useRef(null);
-
+    const ballRef = useRef(null);
+   
     useEffect(() => {
         const ball = ballRef.current;
         const container = containerRef.current;
@@ -12,6 +13,7 @@ export default function Basketball() {
         const containerWidth = container.clientWidth;
         const containerHeight = container.clientHeight;
 
+        // The next 4 lines initialize the position and velocity of the ball
         let x = Math.floor(Math.random() * (containerWidth - 200));
         let y = Math.floor(Math.random() * (containerHeight - 200));
         let vx = Math.random() * 2 + 1;
@@ -19,13 +21,16 @@ export default function Basketball() {
 
         function movement() {
             requestAnimationFrame(movement);
-
+            
+            // Checks for boundary width, reverses mvmt if ball reaches boundary
             if (x + 200 > containerWidth || x < 0) vx = -vx;
             if (y + 200 > containerHeight || y < 0) vy = -vy;
 
+            // Updates position of ball
             x += vx;
             y += vy;
 
+            // Applies new position to the ball element
             ball.style.transform = `translate(${x}px, ${y}px)`;
         }
         movement();
