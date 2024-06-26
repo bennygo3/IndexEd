4e84f687c4babdd7034ddb87a771a4efab540013
bennygo3 <3gobenjamin@gmail.com>
2024-06-25 19:27:57 -0500

    basics of tic tac toe are complete... thanks react. Now to implement playing vs computer or play vs friend game mode

--- client/src/components/TTT/TTT.js
+++ client/src/components/TTT/TTT.js
@@ -9,12 +9,32 @@ function Square({ value, onSquareClick }) {
     );
 }
 
+function calculateWinner(squares) {
+    const wLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+    ];
+    for (let i = 0; i < wLines.length; i++) {
+        const [a, b, c] = wLines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 export default function Board() {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
-        if (squares[i]) {
+        if (squares[i] || calculateWinner(squares)) {
             return;
         }
         const nextSquares = squares.slice();
@@ -26,9 +46,18 @@ export default function Board() {
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
     }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
     
     return (
         <section className='board-body'>
+            <div className='status'>{status}</div>
             <div className='board-row'>
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
