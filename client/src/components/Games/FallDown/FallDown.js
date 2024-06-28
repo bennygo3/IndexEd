540078bcef106e72735ff397d4b3021ea797cba1
bennygo3 <3gobenjamin@gmail.com>
2024-06-28 16:59:48 -0500

    switching up falldown logic. turned the key down for arrow keys into a continuous motion

--- client/src/components/Games/FallDown/FallDown.js
+++ client/src/components/Games/FallDown/FallDown.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import './fall-down.css';
 
 function Ball({ x, y }) {
-    return <div className='ball' style={{ left: x, top: y }} />
+    return <div className='ball' style={{ left:`${x}%`, top: `${y}%` }} />;
 }
 
 function Floor({ x, y }) {
@@ -12,33 +12,51 @@ function Floor({ x, y }) {
 export default function FallDown() {
     const [ballX, setBallX] = useState(50);
     const [ballY, setBallY] = useState(1);
+    const [moveLeft, setMoveLeft] = useState(false);
+    const [moveRight, setMoveRight] = useState(false);
     const [floors, setFloor] = useState([{ x: 0, y: 100 }, { x: 50, y: 200 }]);
     const [gameOver, setGameOver] = useState(false);
 
     useEffect(() => {
         const handleKeyDown = (e) => {
-            if (e.key === 'ArrowLeft') setBallX((prev) => Math.max(0, prev - 10));
-            if (e.key === 'ArrowRight') setBallX((prev) => Math.min(90, prev + 10));
+            if (e.key === 'ArrowLeft') setMoveLeft(true);
+            if (e.key === 'ArrowRight') setMoveRight(true);
         };
 
+        const handleKeyUp = (e) => {
+            if (e.key === 'ArrowLeft') setMoveLeft(false);
+            if (e.key === 'ArrowRight') setMoveRight(false);
+        }
+
         window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('keyup', handleKeyUp);
 
         const interval = setInterval(() => {
+            //setBallY function is using a function to simulate falling
             setBallY((prev) => prev + 2);
+            
+            if (moveLeft) {
+                setBallX((prev) => Math.max(0, prev - 2))
+            }
+            if (moveRight) {
+                setBallX((prev) => Math.min(90, prev + 2))
+            }
+
             setFloor((prev) => 
             prev.map((floor) => ({ ...floor, y: floor.y - 2 }))
         );
 
         // implement if ball collides with barriers here
 
-        if (ballY > 100) setGameOver(true);
+        if (ballY <= 0) setGameOver(true);
         }, 50);
 
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keyup', handleKeyUp);
             clearInterval(interval);
         };
-    }, [ballY, floors]);
+    }, [moveLeft, moveRight]);
 
     if (gameOver) return <div>Game Over</div>;
 
@@ -51,4 +69,9 @@ export default function FallDown() {
         </div>
     );
 }
+
+        // const handleKeyDown = (e) => {
+        //     if (e.key === 'ArrowLeft') setBallX((prev) => Math.max(0, prev - 2));
+        //     if (e.key === 'ArrowRight') setBallX((prev) => Math.min(90, prev + 2));
+        // };
 
