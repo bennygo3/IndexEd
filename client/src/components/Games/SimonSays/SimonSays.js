0b5d961e6fffab44693bc36ce8a7c6ec79ec7101
bennygo3 <3gobenjamin@gmail.com>
2024-07-31 15:06:23 -0500

    preliminary hooks and beginning structure of simon game implemented

--- client/src/components/Games/SimonSays/SimonSays.js
+++ client/src/components/Games/SimonSays/SimonSays.js
@@ -8,4 +8,38 @@ export default function SimonSays() {
     const [sequence, setSequence] = useState([]);
     const [userSequence, setUserSequence] = useState([]);
     const [isUserTurn, setIsUserTurn] = useState(false);
+    const [message, setMessage] = useState('Push start to play Simon Says!')
+
+    useEffect(() => {
+        if (sequence.length > 0 && userSequence.length === sequence.length) {
+            checkUserSequence();
+        }
+    }, [userSequence]);
+
+    const startGame = () => {
+        setSequence([]);
+        setUserSequence([]);
+        setIsUserTurn([]);
+        addToSequence();
+    };
+
+    const addToSequence = () => {
+        const newColor = colors[Math.floor(Math.random() * 4)];
+        setSequence([...sequence, newColor]);
+        setUserSequence([]);
+        setIsUserTurn(false);
+        setMessage('Simon says:');
+    };
+
+    const handleColorClick = (color) => {
+        if (!isUserTurn) return;
+        setUserSequence([...userSequence, color]);
+    }
+
+    const checkUserSequence = () => {
+        if (userSequence.join('') === sequence.join('')) {
+            
+        }
+    }
+
 }
