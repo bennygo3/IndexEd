d4d8d49eac371904d58a0f4dbbfbd96818935cef
bennygo3 <3gobenjamin@gmail.com>
2024-09-25 16:43:22 -0500

    saving minor changes and clean up and notes before switching the layout of the home page to a grid

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,18 +3,19 @@ import Navbar from '../../components/Navbar/Navbar';
 import LineGenerator from '../../components/Lines/LineGenerator.js';
 import LogoutButton from '../../components/Logout/LogoutButton.js';
 
-export default function Home(){
+export default function Home() {
   return (
     <main className="home-body">
       <h1 className="home-header">Index<span style={{ marginLeft: "10px" }}></span>Ed</h1>
-      <div className="home-container">
-        <div className="home-background">
+
+      <div className="home-background">
         <LogoutButton className='home-logout' />
-          <div className="red-line"></div>
-          <LineGenerator amount={17} colorClass="blue-line" />
-          <Navbar className="navbar-home"/>
-        </div>
+        {/* <button className="login-button">Login</button> */}
+        <div className="red-line"></div>
+        <LineGenerator amount={17} colorClass="blue-line" />
+        <Navbar className="navbar-home" />
       </div>
+
     </main>
   );
 };
