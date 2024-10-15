8c564b347cfcb55e54509db81be1c17d598612af
bennygo3 <3gobenjamin@gmail.com>
2024-10-15 15:55:49 -0500

    trying to figure out how i want to style my study cards section

--- client/src/pages/MyCards/MyCards.js
+++ client/src/pages/MyCards/MyCards.js
@@ -5,10 +5,13 @@ import Card from '../../components/Card/Card';
 import './MyCards.css';
 
 export default function MyCards() {
-    const [isFlipped, setIsFlipped] = useState(false);
+    const [flippedCard, setFlippedCard] = useState({});
 
-    const handleFlip = () => {
-        setIsFlipped(prevState => !prevState);
+    const handleFlip = (cardId) => {
+        setFlippedCard(prevState => ({
+            ...prevState,
+            [cardId]: !prevState[cardId]
+        }));
     };
     const { loading, error, data } = useQuery(GET_CURRENT_USER);
 
@@ -20,15 +23,20 @@ export default function MyCards() {
             {data.currentUser.stacks.map(stack => (
                 <div key={stack._id} className="stack-section">
                     <h2>{stack.title}</h2>
-                    <div className="my-card-carousel" onClick={handleFlip}>
+                    <div className="my-cards-carousel">
                         {stack.studycards.map(card => (
+                            <div
+                                key={card._id}
+                                className="my-cards-container"
+                                onClick={() => handleFlip(card._id)}
+                            >
                             <Card 
-                                
-                                key={card._id} 
+                                // key={card._id} 
                                 front={card.front} 
                                 back={card.back} 
-                                isFlipped={isFlipped}
+                                isFlipped={flippedCard[card._id] || false} // default to false if not flipped yet
                             />
+                            </div>
                         ))}
                     </div>
                 </div>
