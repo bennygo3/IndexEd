1d74a10b0af473330a920cec4979139305443b7e
bennygo3 <3gobenjamin@gmail.com>
2025-06-23 13:48:23 -0500

    baseline logic for chess. debugging begins

--- client/src/pages/Games/Chess/ChessBoard.jsx
+++ client/src/pages/Games/Chess/ChessBoard.jsx
@@ -1,89 +1,71 @@
 import React, { useState } from 'react';
 import Square from './Square';
 import Piece from './Piece';
+import { isValidMove } from './utils/helpers';
 import './chessGame.css';
 
 const startingBoard = [
-    ['wr','wn','wb','wq','wk','wb','wn','wr'],
-    ['wp','wp','wp','wp','wp','wp','wp','wp'],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['bp','bp','bp','bp','bp','bp','bp','bp'],
-    ['br','bn','bb','bq','bk','bb','bn','br']
+    ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'],
+    ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+    ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br']
 ];
 
 const Chess = () => {
     const [boardState, setBoardState] = useState(startingBoard)
     const [selected, setSelected] = useState(null);
+    const [currentTurn, setCurrentTurn] = useState('white');
 
     const handlePieceClick = (chessRow, chessCol) => {
         const selectedPiece = boardState[chessRow][chessCol];
 
         if (selected) {
+            // const from = { row: selected.chessRow, col: selected.chessCol };
             const from = selected;
-            const to = { chessRow, chessCol };
+            const to = { chessRow, chessCol }
 
-            const newBoard = [...boardState.map(r => [...r])];
-            newBoard[to.chessRow][to.chessCol] = boardState[from.chessRow][from.chessCol];
-            newBoard[from.chessRow][from.chessCol] = '';
-            setBoardState(newBoard);
-            setSelected(null);
-        } else if (selectedPiece) {
-            setSelected({ chessRow, chessCol });
+            if (isValidMove(from, to, boardState, currentTurn)) {
+                const newBoard = [...boardState.map(r => [...r])];
+                newBoard[to.chessRow][to.chessCol] = boardState[from.chessRow][from.chessCol];
+                newBoard[from.chessRow][from.chessCol] = '';
+                setBoardState(newBoard);
+                setSelected(null);
+                setCurrentTurn(currentTurn === 'white' ? 'black' : 'white');
+            } else {
+                setSelected(null);
+            }
+        } else if (selectedPiece && selectedPiece.startsWith(currentTurn[0])) {
+            setSelected({ chessRow, chessCol})
         }
     };
 
     return (
         <>
-        <div className="top-border"></div>
-        <div className="chessboard">
-            {boardState.flatMap((rowArr, rowIndex) => 
-                rowArr.map((piece, colIndex) => {
-                    const isDark = (rowIndex + colIndex) % 2 === 1;
+            <div className="top-border"></div>
+            <div className="chessboard">
+                {boardState.flatMap((rowArr, rowIndex) =>
+                    rowArr.map((piece, colIndex) => {
+                        const isDark = (rowIndex + colIndex) % 2 === 1;
 
-                    return (
-                        <Square 
-                            key={`${rowIndex}-${colIndex}`}
-                            isDark={isDark}
-                            position={{ chessRow: rowIndex, chessCol: colIndex }}
-                            onClick={() => handlePieceClick(rowIndex, colIndex)}
-                        >
-                            {piece && <Piece type={piece} />}
-                        </Square>
-                    );
-                })
-            )}
-        </div>
+                        return (
+                            <Square
+                                key={`${rowIndex}-${colIndex}`}
+                                isDark={isDark}
+                                position={{ chessRow: rowIndex, chessCol: colIndex }}
+                                onClick={() => handlePieceClick(rowIndex, colIndex)}
+                            >
+                                {piece && <Piece type={piece} />}
+                            </Square>
+                        );
+                    })
+                )}
+            </div>
         </>
     );
 };
 
 export default Chess;
-
-//  <>
-//         <div className="top-border"></div>
-//         <div className="chessboard">
-//             {boardState.map((rowArr, rowIndex) => (
-//                 <div className="chessRow" key={rowIndex}>
-//                     {rowArr.map((piece, colIndex) => {
-//                         const isDark = (rowIndex + colIndex) % 2 === 1;
-
-//                         return (
-//                             <Square 
-//                                 key = {`${rowIndex}-${colIndex}`}
-//                                 isDark = {isDark}
-//                                 position = {{ chessRow: rowIndex, chessCol: colIndex }}
-//                                 onClick = {() => handlePieceClick(rowIndex, colIndex)}
-//                             >
-//                                 {piece && <Piece type = {piece} />}
-//                             </Square>
-                            
-//                         );
-//                     })}
-//                 </div>
-                
-//             ))}
-//         </div>
-//         </>
--- client/src/pages/Games/Chess/utils/helpers.js
+++ client/src/pages/Games/Chess/utils/helpers.js
@@ -1,5 +1,5 @@
 export function isValidMove(from, to, board, currentTurn) {
-    const piece = board[from.row][from.col];
+    const piece = board[from.chessRow][from.chessCol];
     if (!piece) return false;
 
     const isWhite = piece.startsWith('w');
@@ -24,13 +24,85 @@ export function isValidMove(from, to, board, currentTurn) {
             return isValidQueenMove(from, to, board);
         case 'k':
             return isValidKingMove(from, to);
-        default: 
+        default:
             return false;
     }
+}
 
-    function isValidPawnMove(from, to, board, isWhite) {
-        const dir = isWhite ? -1 : 1;
-        const startRow = isWhite ? 6 : 1;
-        const target = board[to.row][to.col];
+function isPathClear(from, to, board) {
+    const rowMove = Math.sign(to.chessRow - from.chessRow);
+    const colMove = Math.sign(to.chessCol - from.chessCol);
+
+    let currentRow = from.chessRow + rowMove;
+    let currentCol = from.chessCol + colMove;
+
+    while (currentRow !== to.chessRow || currentCol !== to.chessCol) {
+        if (board[currentRow][currentCol] !== '') return false;
+        currentRow += rowMove;
+        currentCol += colMove;
     }
+
+    return true;
+}
+
+function isValidPawnMove(from, to, board, isWhite) {
+    const dir = isWhite ? -1 : 1;
+    const startRow = isWhite ? 6 : 1;
+    const target = board[to.chessRow][to.chessCol];
+
+    if (from.chessCol === to.chessCol && to.chessRow === from.chessRow + dir && !target) {
+        return true;
+    }
+
+    if (
+        from.chessCol === to.chessCol &&
+        from.chessRow === startRow &&
+        to.chessRow === from.chessRow + 2 * dir &&
+        !target &&
+        !board[from.chessRow + dir][from.chessCol]
+    ) {
+        return true;
+    }
+
+    // Diagonal take pawn
+    if (
+        Math.abs(to.chessCol - from.chessCol) === 1 &&
+        to.chessRow === from.chessRow + dir &&
+        target &&
+        isWhite !== target.startsWith('w')
+    ) {
+        return true;
+    }
+
+    return false;
+}
+
+function isValidRookMove(from, to, board) {
+    if (from.chessRow !== to.chessRow && from.chessCol !== to.chessCol) return false;
+    return isPathClear(from, to, board);
+}
+
+function isValidKnightMove(from, to) {
+    const rowDiff = Math.abs(from.chessRow - to.chessRow);
+    const colDiff = Math.abs(from.chessCol - to.chessCol);
+    return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2);
+}
+
+function isValidBishopMove(from, to, board) {
+    if (Math.abs(from.chessRow - to.chessRow) !== Math.abs(from.chessCol - to.chessCol)) return false;
+    return isPathClear(from, to, board);
+}
+
+function isValidQueenMove(from, to, board) {
+    const isStraight = from.chessRow === to.chessRow || from.chessCol === to.chessCol;
+    const isDiagonal = Math.abs(from.chessRow - to.chessRow) === Math.abs(from.chessCol - to.chessCol);
+
+    if (!isStraight && !isDiagonal) return false;
+    return isPathClear(from, to, board);
+}
+
+function isValidKingMove(from, to) {
+    const rowDiff = Math.abs(from.chessRow - to.chessRow);
+    const colDiff = Math.abs(from.chessCol - to.chessCol);
+    return rowDiff <= 1 && colDiff <= 1;
 }
