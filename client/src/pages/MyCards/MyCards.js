475d2f0fbd412f4b805d4448214311fa7b71b11d
bennygo3 <3gobenjamin@gmail.com>
2025-07-21 12:13:52 -0500

    more minor changes to layout for msc  need to finalize how I want the layout to be

--- client/src/pages/MyCards/MyCards.js
+++ client/src/pages/MyCards/MyCards.js
@@ -32,11 +32,12 @@ export default function MyCards() {
             <header id="my-cards-chalkboard">
                 {/* <div> */}
                     <h1 id="my-cards-header">My Study Cards</h1>
-                    <ul>
-                    <Link to="/" id="mcp-home">Home</Link>
-                    <Link to="/card-create" id="mcp-card-create">Create a new card</Link>
-                    <Link to="/trivia" id="mcp-trivia">Trivia cards</Link>
-                    <Link to="/games" id="mcp-games">Games</Link>
+                    <ul id="my-cards-header-list">
+                        <li><Link to="/" id="mcp-home">Home</Link></li>
+                    
+                    <li><Link to="/card-create" id="mcp-card-create">Create a new card</Link></li>
+                    <li><Link to="/trivia" id="mcp-trivia">Trivia cards</Link></li>
+                    <li><Link to="/games" id="mcp-games">Games</Link></li>
                     </ul>
                     <div id="my-cards-chalk"></div>
                 {/* </div> */}
