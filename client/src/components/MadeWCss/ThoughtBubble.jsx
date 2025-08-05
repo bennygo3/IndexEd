d9e120f7f1a2c3e0cde7fc818c64f09468db61da
bennygo3 <3gobenjamin@gmail.com>
2025-08-05 13:21:20 -0500

    css changes to thought bubble component. info for user is now showing inside the buble

--- client/src/components/MadeWCss/ThoughtBubble.jsx
+++ client/src/components/MadeWCss/ThoughtBubble.jsx
@@ -4,7 +4,9 @@ import "./thoughtBubble.css";
 const ThoughtBubble = () => {
     return (
         <div className="thought-bubble-container"> 
-            <div className="main-bubble">No material to study yet. Start by creating some study cards for review!</div> 
+            <div className="main-bubble"></div>
+            <div className="main-bubble-text">No material to study yet. Start by creating some study cards for review!</div> 
+            {/* <div className="main-bubble">No material to study yet. Start by creating some study cards for review!</div>  */}
             <div className="small-bubble one" />
             <div className="small-bubble two" />
         </div>
