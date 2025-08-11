c843542c0c6332383d2c3685b8d364e7320abf12
bennygo3 <3gobenjamin@gmail.com>
2025-08-11 14:44:52 -0500

    learning to make a cloud and made a cloud with different box shadows and pseudo elements

--- client/src/components/MadeWCss/ThoughtBubble.jsx
+++ client/src/components/MadeWCss/ThoughtBubble.jsx
@@ -5,10 +5,9 @@ const ThoughtBubble = () => {
     return (
         <div className="thought-bubble-container"> 
             <div className="main-bubble"></div>
-            <div className="main-bubble-text">No material to study yet. Start by creating some study cards for review!</div> 
-            {/* <div className="main-bubble">No material to study yet. Start by creating some study cards for review!</div>  */}
-            <div className="small-bubble one" />
-            <div className="small-bubble two" />
+            <div className="main-bubble-text">No material to study yet. Start by creating some study cards!</div> 
+            {/* <div className="small-bubble one" /> */}
+            {/* <div className="small-bubble two" /> */}
         </div>
     );
 };
