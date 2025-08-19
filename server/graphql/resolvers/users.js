6c9d3289f3ceae71a014cedd7dba1c39725c2b4d
bennygo3 <3gobenjamin@gmail.com>
2025-08-19 12:01:43 -0500

    updated user resolver

--- server/graphql/resolvers/users.js
+++ server/graphql/resolvers/users.js
@@ -18,14 +18,14 @@ const Query = {
                 populate: {
                     path: 'studyCards',
                     model: 'StudyCard',
-                }, 
+                },
             })
             .populate({
                 path: 'studyCards',
                 model: 'StudyCard',
             })
             .populate('snakeScores')
-        
+
         if (!user) {
             throw new GraphQLError('User not found!', {
                 extensions: { code: 'BAD_USER_INPUT' },
@@ -102,60 +102,88 @@ const Mutation = {
 
     async updateHighSnakeScore(_, { newSnakeScore }, context) {
         const authId = context.user?._id;
-        if(!authId) {
-            throw new GraphQLError('Not authenticated' , {
-                extensions: {code: 'UNAUTHENTICATED' },
+        if (!authId) {
+            throw new GraphQLError('Not authenticated', {
+                extensions: { code: 'UNAUTHENTICATED' },
             });
         }
 
         if (!Number.isInteger(newSnakeScore) || newSnakeScore < 0) {
             throw new GraphQLError('Score must be a non-negative integer', {
-        extensions: { code: 'BAD_USER_INPUT' },
-        });
+                extensions: { code: 'BAD_USER_INPUT' },
+            });
+        }
+
+        const user = await Users.findById(authId);
+        if (!user) {
+            throw new GraphQLError('User not found', {
+                extensions: { code: 'BAD_USER_INPUT' },
+            });
+        }
+
+        // create a new high score in snake for a user if they are signed in and play for the first time
+        const existing = await SnakeScore.findOne({ userId: authId });
+
+        if (!existing) {
+            await SnakeScore.create({
+                userId: authId,
+                username: user.username,
+                highScore: newSnakeScore,
+            });
+            return { changed: true, highScore: newSnakeScore };
+        }
+
+        if (newSnakeScore > existing.highScore) {
+            existing.highScore = newSnakeScore;
+            existing.username = user.username;
+            await existing.save();
+            return { changed: true, highScore: existing.highScore };
+        }
+
+        return { changed: false, highScore: existing.highScore }
     }
-
-    const snakeUpdated = await SnakeScore.findOneAndUpdate(
-        { userId: authId },
-        {
-            $setOnInsert: { userId: authId, username: user.username, highScore: newSnakeScore },
-            $max: { highScore: newSnakeScore },
-            $set: { username: user.username },
-        },
-        { new: true, upsert: true }
-    );
-
-    return snakeUpdated;
-    },
 };
 
 export default { Query, Mutation };
 
 //async getHighScoreSnake(_, { userId }) {
 
-    //     const snakeScore = await SnakeScore.findOne({ userId });
+//     const snakeScore = await SnakeScore.findOne({ userId });
 
-    //     if (!snakeScore) {
-    //         return null;
-    //     }
+//     if (!snakeScore) {
+//         return null;
+//     }
 
-    //     const username = snakeScore.username || "Unknown";
+//     const username = snakeScore.username || "Unknown";
 
-    //     return {
-    //         _id: snakeScore._id,
-    //         userId: snakeScore.userId,
-    //         username: username,
-    //         highScore: snakeScore.highScore,
-    //     };
-    // },
-        // async updateHighSnakeScore(_, { userId, newSnakeScore }) {
-    //     const highSnakeScore = await SnakeScore.findOne({ userId });
+//     return {
+//         _id: snakeScore._id,
+//         userId: snakeScore.userId,
+//         username: username,
+//         highScore: snakeScore.highScore,
+//     };
+// },
+// async updateHighSnakeScore(_, { userId, newSnakeScore }) {
+//     const highSnakeScore = await SnakeScore.findOne({ userId });
 
-    //     if (!highSnakeScore) {
-    //         const user = await Users.findById(userId);
-    //         if(!user) {
-    //             throw new GraphQLError('User not found', { extensions: { code: 'BAD_USER_INPUT' } });
-    //         }
+//     if (!highSnakeScore) {
+//         const user = await Users.findById(userId);
+//         if(!user) {
+//             throw new GraphQLError('User not found', { extensions: { code: 'BAD_USER_INPUT' } });
+//         }
 
-    //         const 
-    //     }
-    // }
+//         const
+//     }
+// }
+
+// const snakeUpdated = await SnakeScore.findOneAndUpdate(
+        //     { userId: authId },
+        //     {
+        //         $setOnInsert: { userId: authId, username: user.username, highScore: newSnakeScore },
+        //         $max: { highScore: newSnakeScore },
+        //         $set: { username: user.username },
+        //     },
+        //     { new: true, upsert: true }
+        // );
+
+        // return snakeUpdated;
