fb66726bec859a601c050c940b957d0727071826
bennygo3 <95048609+bennygo3@users.noreply.github.com>
2026-06-29 12:19:19 -0500

    adding to nba schema structure

--- server/db-sql/schema.sql
+++ server/db-sql/schema.sql
@@ -1,6 +1,17 @@
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS divisions; -- divisions is dropped before conferences because divisions depends on conferences
+DROP TABLE IF EXISTS conferences; 
+
 CREATE TABLE conferences (
-    id SERIAL PRIMARY KEY, --generates unique integer for every conference (2)
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, --generates unique integer for every conference (2)
+
     name VARCHAR(50) NOT NULL UNIQUE -- varchar50 allows up to 50 chars to be used; not null = every conference must have a name; unique = no conference duplicates allowed
+);
+
+CREATE TABLE divisions (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    name VARCHAR(50) NOT NULL UNIQUE,
 
     conference_id INTEGER NOT NULL,
 
@@ -8,3 +19,22 @@ CREATE TABLE conferences (
         FOREIGN KEY (conference_id) -- The value stored in conference_id must already exist
         REFERENCES conferences(id) -- ... in the id column of the conference table. Meaning = a division cannot be assigned to a conference with an unrecognized conference id
 );
+
+CREATE TABLE teams (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    location VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    abbreviation CHAR(3) NOT NULL UNIQUE,
+
+    logo_url TEXT,
+
+    division_id INTEGER NOT NULL,
+
+    CONSTRAINT uk_team_name
+        UNIQUE (location, name),
+
+    CONSTRAINT fk_team_division
+        FOREIGN KEY (division_id)
+        REFERENCES divisions(id)
+)
