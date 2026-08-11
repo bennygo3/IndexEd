003c43707b1fd3bf188774f130e769251dc02e4d
bennygo3 <95048609+bennygo3@users.noreply.github.com>
2026-08-11 13:39:34 -0500

    logout button now directly records state instead of checking for recently deleted cookies unneccesarily

--- client/src/components/Logout/LogoutButton.js
+++ client/src/components/Logout/LogoutButton.js
@@ -5,16 +5,33 @@ import authService from '../../utils/auth';
 
 export default function LogoutButton({ className }) {
     const navigate = useNavigate();
-    const { checkAuth } = useAuth();
+    
+    const { 
+        setIsLoggedIn,
+        setIsAuthChecked
+    } = useAuth();
 
     const handleLogout = async () => {
-       await  authService.logout();
-       await checkAuth();
-       navigate("/");
+        try {
+            await  authService.logout();  // Revokes the refresh token and clears the auth cookies
+
+            // update React authentication state directly
+            setIsLoggedIn(false);
+            setIsAuthChecked(true);
+
+            navigate('/');
+        } catch (error) {
+            console.error('Logout error:', error);
+        }
+       
     };
 
     return (
-        <button className={className} onClick={handleLogout}>
+        <button 
+            type="button"
+            className={className} 
+            onClick={handleLogout}
+        >
             Sign<br></br>Out
         </button>
     );
